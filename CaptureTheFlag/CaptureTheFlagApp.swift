f96b44017b5f54463fc9d481e9a66b59fecdddad
sum-lab <sum00ncat@gmail.com>
2021-03-12 15:17:01 +0530

    Initial Commit

---
+++ CaptureTheFlag/CaptureTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureTheFlagApp.swift
+//  CaptureTheFlag
+//
+//  Created by Sumayyah on 12/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CaptureTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
